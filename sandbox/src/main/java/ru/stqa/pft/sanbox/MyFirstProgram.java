c76ff653017b5f4d0ad806f486c5e70c40c29a81
Andrii Leonov <andriileonov74@gmail.com>
2019-05-25 13:01:56 +0300

    Создан тест Создание Новой Группы

--- sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sanbox/MyFirstProgram.java
@@ -4,25 +4,25 @@ import java.awt.*;
 
 public class MyFirstProgram {
 
-   public static void main(String[] args) {   
-     hello("world");
-     hello("user");
-     hello("Andrii");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Andrii");
 
-     Square s = new Square(5);
-     System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-     Rectangle r = new Rectangle(4, 6);
-     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-     Point p1 = new Point(6, 0);
-     Point p2 = new Point(8, 0);
-     System.out.println("Расстояние между точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + p1.distance(p2));
-   }
+    Point p1 = new Point(6, 0);
+    Point p2 = new Point(8, 0);
+    System.out.println("Расстояние между точками " + p1.x + ";" + p1.y + " и " + p2.x + ";" + p2.y + " = " + p1.distance(p2));
+  }
 
-   public static void hello(String somebody) {
-     System.out.println("Hello, " + somebody + "!");
-   }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
 
 
 }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -6,10 +6,10 @@ import ru.stqa.pft.sanbox.Point;
 
 public class PointTests {
 
-    @Test
-    public void testDistance() {
-      Point p1 = new Point(6, 0);
-      Point p2 = new Point(8, 0);
-      Assert.assertEquals(p1.distance(p2), 2.0);
-    }
+  @Test
+  public void testDistance() {
+    Point p1 = new Point(6, 0);
+    Point p2 = new Point(8, 0);
+    Assert.assertEquals(p1.distance(p2), 2.0);
+  }
 }
